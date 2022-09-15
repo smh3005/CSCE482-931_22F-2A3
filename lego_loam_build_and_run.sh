4179ba5f423054c34ae8f325fa89164e41710dff
Kai Gomes <kaigomes7@tamu.edu>
2022-09-14 19:40:51 -0500

    Added build and run script for lego_loam framework

---
+++ lego_loam_build_and_run.sh
@@ -0,0 +1,8 @@
+cd ~
+mkdir -p catkin_ws/src
+cd catkin_ws/src
+git clone https://github.com/kaigomes7/LeGO-LOAM.git
+cd ..
+catkin_make -j1
+source devel/source.bash
+roslaunch lego_loam run.launch
